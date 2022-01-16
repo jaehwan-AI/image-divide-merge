4f71c4cd8d89832a0bb3506dbca88e67ba6b3daf
jaehwan-AI <jaehwan0995@gmail.com>
2022-01-16 18:58:19 +0900

    [FEAT] add test code

---
+++ test_solver.py
@@ -0,0 +1,31 @@
+import argparse
+import json
+import os
+
+import albumentations as A
+import cv2
+import numpy as np
+
+from model import AutoEncoder
+from puzzle_solver import (fit_knn_model, generate_embedding_from_model,
+                           generate_mapping_list, generate_result_image,
+                           unscramble_image)
+
+
+def merge(args):
+    fold_name = args.prefix_input
+    json_object = json.load(open(f'{fold_name}/state.json', 'r'))
+
+    
+    model = AutoEncoder()
+    raw_embedding_flatten, transformed_embedding_flatten = generate_embedding_from_model(model, raw_image, transformed_image, output_shape_model)
+
+    knn_neighbors = 5
+    knn_metric = "cosine"
+    knn = fit_knn_model(raw_embedding_flatten, knn_neighbors, knn_metric)
+
+    final_list_map = generate_mapping_list(knn, )
+    final_image_np = generate_result_image()
+    
+    cv2.imwrite('output.jpg', np.array(img_tiles))
+    print("Success merged!!")
